52aad940079226ca651cdbae9c794e003458cf38
johrulit@gmail.com <johrulit@gmail.com>
2021-06-12 02:05:40 +0600

    Update script.js

--- output/script.js
+++ output/script.js
@@ -1,3 +1,9 @@
 "use strict";
 console.log("Hello World!");
 console.log("I love bangladesh");
+function ishaal(a) {
+    return a;
+}
+console.log("I love " + ishaal("Ishaal"));
+var bp = function (b) { return b; };
+console.log("I like " + bp("Bipasha"));
